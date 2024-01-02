31234617a76b3129567bdeaa26d99852fb2ab3a7
AnderCMD <107501858+AnderCMD@users.noreply.github.com>
2024-01-01 20:12:30 -0600

    Formateo de Codigo

--- Interactive-Rating-Component-Main/JavaScript/Rate.js
+++ Interactive-Rating-Component-Main/JavaScript/Rate.js
@@ -1,23 +1,23 @@
 let CurrentRate = 0; // Variable global
 
 function Rate(SelectRate) {
-    CurrentRate = SelectRate; // Almacenar el rate en la variable
+	CurrentRate = SelectRate; // Almacenar el rate en la variable
 
-    for (let i = 1; i <= 5; i++) {
-        const Button = document.getElementById("Rate" + i);
-        Button.classList.remove("RateActivo");
-    }
+	for (let i = 1; i <= 5; i++) {
+		const Button = document.getElementById('Rate' + i);
+		Button.classList.remove('RateActivo');
+	}
 
-    for (let i = 1; i <= SelectRate; i++) {
-        const Button = document.getElementById("Rate" + i);
-        Button.classList.add("RateActivo");
-    }
+	for (let i = 1; i <= SelectRate; i++) {
+		const Button = document.getElementById('Rate' + i);
+		Button.classList.add('RateActivo');
+	}
 }
 
 function Enviar() {
-    // Guardar currentRate en localStorage
-    localStorage.setItem('Rate', CurrentRate);
+	// Guardar currentRate en localStorage
+	localStorage.setItem('Rate', CurrentRate);
 
-    // Redirigir a la nueva página
-    window.location.href = 'Thanks.html'; // Cambia 'Thanks.html' a la URL de tu nueva página
+	// Redirigir a la nueva página
+	window.location.href = 'Thanks.html'; // Cambia 'Thanks.html' a la URL de tu nueva página
 }
